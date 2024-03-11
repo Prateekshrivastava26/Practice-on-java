3396ddde9ee9c3fdbe476e773e9b203023b7db7c
Prateekshrivastava26 <130172863+Prateekshrivastava26@users.noreply.github.com>
2024-03-11 15:01:33 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class name {
+    public static void main(String[] args) {
+        Scanner harry = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String a = harry.nextLine();
+        System.out.println(a);
+    }}
